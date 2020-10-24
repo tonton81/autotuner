a9da85d2c88521df3d519c4f74c34951156bd453
Antonio Brewer <yodabox64@hotmail.com>
2020-10-24 11:05:59 -0400

    Update autotuner_config.py

--- autotuner_config.py
+++ autotuner_config.py
@@ -79,12 +79,29 @@ if len(sys.argv) > 1:
 
 
   for i in range(1, len(sys.argv)):
+
+    if sys.argv[i] == "DEFAULT_LIST":
+      try:
+        BPV_list = "[[0, 2560, 8000], [0, 2560, 3840]]"
+        config_data['torqueBPV'] = str(BPV_list)
+      except KeyError:
+        continue
+
+    if sys.argv[i] == "SET.TONY":
+      try:
+        BPV_list = "[[0, 2327, 3525, 4119, 4511, 5131, 10435, 21091, 23755], [0, 512, 768, 1144, 1516, 2048, 2560, 3584, 3840]]"
+        config_data['torqueBPV'] = str(BPV_list)
+      except KeyError:
+        continue
+
     if sys.argv[i] == "RESET":
       autotuner_config.resetall_config()
       raise SystemExit()
+
     if sys.argv[i] == "BASH_NEW_QUEUE":
       autotuner_config.bash_new_queue(sys.argv[2])
       raise SystemExit()
+
     if sys.argv[i] == "SET.BP":
       try:
         BPV_list = eval(config_data['torqueBPV'])
@@ -92,6 +109,7 @@ if len(sys.argv) > 1:
         config_data['torqueBPV'] = str(BPV_list)
       except KeyError:
         continue
+
     if sys.argv[i] == "SET.V":
       try:
         BPV_list = eval(config_data['torqueBPV'])
@@ -99,14 +117,61 @@ if len(sys.argv) > 1:
         config_data['torqueBPV'] = str(BPV_list)
       except KeyError:
         continue
+
     if sys.argv[i] == "SET.MP":
       try:
         BPV_list = eval(config_data['torqueBPV'])
+        if ( len(BPV_list[0]) != 3 ):
+          print ("Only 3 value lists are supported")
+          continue
+        if ( len(BPV_list[1]) != 3 ):
+          print ("Only 3 value lists are supported")
+          continue
         BPV_list[0][1] = round(float(sys.argv[i+1]))
         BPV_list[1][1] = round(float(sys.argv[i+1]))
         config_data['torqueBPV'] = str(BPV_list)
       except KeyError:
         continue
+
+    if sys.argv[i] == "SET.MP1":
+      try:
+        if ( len(BPV_list[0]) != 3 ):
+          print ("Only 3 value lists are supported")
+          continue
+        BPV_list = eval(config_data['torqueBPV'])
+        BPV_list[0][1] = round(float(sys.argv[i+1]))
+        config_data['torqueBPV'] = str(BPV_list)
+      except KeyError:
+        continue
+
+    if sys.argv[i] == "SET.MP2":
+      try:
+        if ( len(BPV_list[1]) != 3 ):
+          print ("Only 3 value lists are supported")
+          continue
+        BPV_list = eval(config_data['torqueBPV'])
+        BPV_list[1][1] = round(float(sys.argv[i+1]))
+        config_data['torqueBPV'] = str(BPV_list)
+      except KeyError:
+        continue
+
+
+    if sys.argv[i] == "INC.MP1":
+      try:
+        BPV_list = eval(config_data['torqueBPV'])
+        if ( len(BPV_list[0]) != 3 ):
+          print ("Only 3 value lists are supported")
+          continue
+        BPV_list[0][1] = BPV_list[0][1] + int(sys.argv[i+1])
+        config_data['torqueBPV'] = str(BPV_list)
+        KpKi_list = eval(config_data['pidKpKi'])
+        print ("\n  New tune set!\n    " + str(BPV_list) + "\n    " + str(KpKi_list) + "\n")
+      except KeyError:
+        continue
+
+
+
+
     if sys.argv[i] == "SET.KP":
       try:
         KpKi_list = eval(config_data['pidKpKi'])
@@ -114,6 +179,7 @@ if len(sys.argv) > 1:
         config_data['pidKpKi'] = str(KpKi_list)
       except KeyError:
         continue
+
     if sys.argv[i] == "SET.KI":
       try:
         KpKi_list = eval(config_data['pidKpKi'])
@@ -121,11 +187,13 @@ if len(sys.argv) > 1:
         config_data['pidKpKi'] = str(KpKi_list)
       except KeyError:
         continue
+
     if sys.argv[i] == "SET.KPDIV":
       try:
         KpDIV = float(sys.argv[i+1])
       except KeyError:
         continue
+
     if sys.argv[i] == "TUNEKP":
       try:
         KpKi_list = eval(config_data['pidKpKi'])
@@ -149,8 +217,9 @@ if len(sys.argv) > 1:
       KpKi_list = eval(config_data['pidKpKi'])
       KP_DIV = float(KpDIV) if "KPDIV" in str(sys.argv) else float(3)
       if not "SET.BP" in str(sys.argv):
-        BPV_list[0][-1] = round(float(BPV_list[1][-1]) * 0.8 / float(KpKi_list[0][0]))
-        config_data['torqueBPV'] = str(BPV_list)
+        if ( len(BPV_list[0]) == 3 ):
+          BPV_list[0][-1] = round(float(BPV_list[1][-1]) * 0.8 / float(KpKi_list[0][0]))
+          config_data['torqueBPV'] = str(BPV_list)
       KpKi_list[1][0] = round(float(KpKi_list[0][0]) / KP_DIV, 5)
       config_data['pidKpKi'] = str(KpKi_list)
     if "SET.BP" in str(sys.argv) or \
